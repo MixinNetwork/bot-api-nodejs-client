97fc7a57e9ab9be8e726996195688febdf613dfa
LiYuqing <im.yuqlee@gmail.com>
2022-05-16 00:04:27 +0800

    Add MVMThreshold

--- src/mvm/registry.ts
+++ src/mvm/registry.ts
@@ -20,6 +20,7 @@ export const MVMTestnet = {
     Address: '0x07B0bF340765CAE77b734D82EB8d35229796CeBc',
   },
   MVMMembers: ['a15e0b6d-76ed-4443-b83f-ade9eca2681a', 'b9126674-b07d-49b6-bf4f-48d965b2242b', '15141fe4-1cfd-40f8-9819-71e453054639', '3e72ca0c-1bab-49ad-aa0a-4d8471d375e7'],
+  MVMThreshold: 3,
 };
 
 const provider = (uri: string) => new StaticJsonRpcProvider(uri);
