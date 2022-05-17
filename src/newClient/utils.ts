25357db59b9788aad4820bee1080c572f859067b
LiYuqing <im.yuqlee@gmail.com>
2022-05-17 16:48:45 +0800

    Add signAuthenticationToken

--- src/newClient/utils.ts
+++ src/newClient/utils.ts
@@ -1,3 +1,8 @@
+import forge from 'node-forge';
+import { v4 as uuidv4 } from 'uuid';
+import serialize from 'serialize-javascript';
+import { Keystore } from './types/keystore';
+
 // Utils is some helper methods for mixin api
 class Utils {
   // base64RawURLEncode is the standard raw, unpadded base64 encoding
@@ -18,6 +23,46 @@ class Utils {
     let data = raw instanceof Buffer ? raw.toString() : raw;
     data = data.replaceAll('-', '+').replaceAll('_', '/');
     return Buffer.from(data, 'base64');
+  }
+
+  static signAuthenticationToken(requestMethod: string, uri: string, params: Object | string, keystore: Keystore) {
+    const method = requestMethod.toLocaleUpperCase();
+    let data: string = '';
+    if (typeof params === 'object') {
+      data = serialize(params);
+    } else if (typeof params === 'string') {
+      data = params;
+    }
+
+    const iat = Math.floor(Date.now() / 1000);
+    const exp = iat + 3600;
+    const md = forge.md.sha256.create();
+    md.update(method + uri + data, 'utf8');
+
+    const payload = {
+      uid: keystore.user_id,
+      sid: keystore.session_id,
+      iat,
+      exp,
+      jti: uuidv4(),
+      sig: md.digest().toHex(),
+      scp: keystore.scope || 'FULL',
+    };
+
+    const header = this.base64RawURLEncode(serialize({ alg: 'EdDSA', typ: 'JWT' }));
+    const payloadStr = this.base64RawURLEncode(serialize(payload));
+
+    const privateKey = this.base64RawURLDecode(keystore.private_key);
+    const result = [header, payloadStr];
+    const signData = forge.pki.ed25519.sign({
+      message: result.join('.'),
+      encoding: 'utf8',
+      privateKey,
+    });
+
+    const sign = this.base64RawURLEncode(Buffer.from(signData));
+    result.push(sign);
+    return result.join('.');
   }
 }
 
