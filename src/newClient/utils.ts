a0366fb4770a76817dfe6c11d819489333b0e5e3
LiYuqing <im.yuqlee@gmail.com>
2022-05-17 14:30:17 +0800

    Support Buffer & string for base64

--- src/newClient/utils.ts
+++ src/newClient/utils.ts
@@ -3,12 +3,20 @@ class Utils {
   // base64RawURLEncode is the standard raw, unpadded base64 encoding
   // base64RawURLDecode is same as encode
   // like Golang version https://pkg.go.dev/encoding/base64#Encoding
-  static base64RawURLEncode(buf: Buffer) {
+  static base64RawURLEncode(raw: Buffer | string): string{
+    let buf = raw;
+    if (typeof raw === 'string') {
+      buf = Buffer.from(raw);
+    }
+    if (buf.length === 0) {
+      return '';
+    }
     return buf.toString('base64').replaceAll('=', '').replaceAll('+', '-').replaceAll('/', '_');
   }
 
-  static base64RawURLDecode(raw: string): Buffer {
-    const data = raw.replaceAll('-', '+').replaceAll('_', '/');
+  static base64RawURLDecode(raw: string | Buffer): Buffer {
+    let data = raw instanceof Buffer ? raw.toString() : raw;
+    data = data.replaceAll('-', '+').replaceAll('_', '/');
     return Buffer.from(data, 'base64');
   }
 }
--- test/mixin/utils.test.ts
+++ test/mixin/utils.test.ts
@@ -2,11 +2,19 @@ import Utils from '../../src/newClient/utils';
 
 describe('Tests for utils', () => {
   test('base64 encode & decode should be url safe', () => {
+    // buffer to base64
     expect(Utils.base64RawURLEncode(Buffer.from('a'))).toMatch('YQ');
     expect(Utils.base64RawURLEncode(Buffer.from('ab'))).toMatch('YWI');
     expect(Utils.base64RawURLEncode(Buffer.from('abcde'))).toMatch('YWJjZGU');
     expect(Utils.base64RawURLEncode(Buffer.from('abcdefghijklmnopqrstuvwxyz'))).toMatch('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
 
+    // string to base64
+    expect(Utils.base64RawURLEncode('abcdefghijklmnopqrstuvwxyz')).toMatch('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
+
+    // empty string to base64
+    expect(Utils.base64RawURLEncode('')).toMatch('');
+
+    // base64 string to buffer
     let buf = Utils.base64RawURLDecode('YQ');
     expect(buf.toString()).toMatch('a');
     buf = Utils.base64RawURLDecode('YWI');
@@ -14,6 +22,10 @@ describe('Tests for utils', () => {
     buf = Utils.base64RawURLDecode('YWJjZGU');
     expect(buf.toString()).toMatch('abcde');
     buf = Utils.base64RawURLDecode('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
+    expect(buf.toString()).toMatch('abcdefghijklmnopqrstuvwxyz');
+
+    // base64 buffer to string
+    buf = Utils.base64RawURLDecode(Buffer.from('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo'));
     expect(buf.toString()).toMatch('abcdefghijklmnopqrstuvwxyz');
   });
 });
