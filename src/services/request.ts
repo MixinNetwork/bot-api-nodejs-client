a34c454bc08d0d0792d568300ac727865a7daf83
LiYuqing <im.yuqlee@gmail.com>
2022-05-18 18:46:28 +0800

    Revert request

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,6 +1,5 @@
 // @ts-ignore
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { ApiError } from 'newClient/error';
 import { v4 as uuid } from 'uuid';
 import { KeystoreAuth } from '../mixin/keystore';
 import { signRequest } from '../mixin/sign';
@@ -9,31 +8,24 @@ import { Keystore } from '../types';
 
 const hostURL = ['https://mixin-api.zeromesh.net', 'https://api.mixin.one'];
 
-export function request(arg?: string | Keystore, config?: AxiosRequestConfig): AxiosInstance {
-  const isCustomUrl = !!config?.baseURL;
-
+export const request = (keystore?: Keystore, token = ''): AxiosInstance => {
   const ins = axios.create({
     baseURL: hostURL[0],
     headers: { 'Content-Type': 'application/json;charset=UTF-8' },
     timeout: 3000,
-    ...config,
   });
 
-  let token: string | undefined;
-  let k: KeystoreAuth | undefined;
-
-  if (typeof arg === 'string') {
-    token = arg;
-  } else {
-    k = new KeystoreAuth(arg);
-  }
+  let k: KeystoreAuth;
+  if (keystore) k = new KeystoreAuth(keystore);
 
   ins.interceptors.request.use((config: AxiosRequestConfig) => {
     const { method, data } = config;
     const uri = ins.getUri(config);
     const requestID = uuid();
     config.headers['x-request-id'] = requestID;
-    const jwtToken = token || k?.signToken(signRequest(method!, uri, data), requestID) || '';
+    let jwtToken = '';
+    if (token) jwtToken = token;
+    else if (k) jwtToken = k.signToken(signRequest(method!, uri, data), requestID);
     config.headers.Authorization = `Bearer ${jwtToken}`;
     return config;
   });
@@ -41,22 +33,21 @@ export function request(arg?: string | Keystore, config?: AxiosRequestConfig): A
   ins.interceptors.response.use(
     (res: AxiosResponse) => {
       const { data, error } = res.data;
-      if (error) throw new ApiError(error.code, error.description, error.status, error.extra, res.headers['X-Request-Id'], error);
-
+      if (error) {
+        error.request_id = res.headers['x-request-id'];
+        return error;
+      }
       return data;
     },
     async (e: any) => {
       if (['ETIMEDOUT', 'ECONNABORTED'].includes(e.code)) {
-        if (isCustomUrl) return e.config;
-
-        ins.defaults.baseURL = e.config.baseURL === hostURL[0] ? hostURL[1] : hostURL[0];
-        e.config.baseURL = ins.defaults.baseURL;
+        ins.defaults.baseURL = e.config.baseURL = e.config.baseURL === hostURL[0] ? hostURL[1] : hostURL[0];
       }
       await delay();
       return ins(e.config);
     },
   );
   return ins;
-}
+};
 
 export const mixinRequest = request();
