1ae898880d900f2a7cff1d8d447174a6d27e431d
hundredark <hundredark@gmail.com>
2022-05-18 14:18:49 +0800

    Move update methods to UserKeystoreClient

--- src/newClient/user.ts
+++ src/newClient/user.ts
@@ -1,6 +1,7 @@
 import { AxiosInstance } from 'axios';
 import { AuthenticationUserResponse, UserResponse, RelationshipRequest } from './types/user';
 import { buildClient } from './utils/build-client';
+import Keystore from "./types/keystore";
 
 // Methods to manage user's information
 export const UserTokenClient = (axiosInstance: AxiosInstance) => {
@@ -25,15 +26,19 @@ export const UserTokenClient = (axiosInstance: AxiosInstance) => {
 
     // Create a network user, can be created by bot only
     createBareUser: (fullName: string, sessionSecret: string) => axiosInstance.post('/users', { full_name: fullName, session_secret: sessionSecret }),
-
-    // Modify current user's personal name and avatar
-    update: (fullName: string, avatarBase64: string) => axiosInstance.post<unknown, UserResponse>(`/me`, { full_name: fullName, avatar_base64: avatarBase64 }),
-
-    // Manage the relationship between two users
-    updateRelationships: (relationship: RelationshipRequest) => axiosInstance.post<unknown, UserResponse>(`/relationships`, relationship),
   };
 
   return methods;
 };
 
-export const UserClient = buildClient(UserTokenClient);
+export const UserKeystoreClient = (_keystore: Keystore, axiosInstance: AxiosInstance) => {
+  return {
+    // Modify current user's personal name and avatar
+    update: (fullName: string, avatarBase64: string) => axiosInstance.post<unknown, UserResponse>(`/me`, { full_name: fullName, avatar_base64: avatarBase64 }),
+
+    // Manage the relationship between two users
+    updateRelationships: (relationship: RelationshipRequest) => axiosInstance.post<unknown, UserResponse>(`/relationships`, relationship),
+  }
+}
+
+export const UserClient = buildClient(UserTokenClient, UserKeystoreClient);
