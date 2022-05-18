3e950d30e7d6b716fe76311201a17103b695f0ad
YeungKC <I@YeungKC.com>
2022-05-18 17:19:59 +0800

    improve update pin

--- src/newClient/pin.ts
+++ src/newClient/pin.ts
@@ -10,17 +10,14 @@ export const PinKeystoreClient = (keystore: Keystore, axiosInstance?: AxiosInsta
 
   return {
     // Verify a user's PIN
-    verify(pin: string) {
+    verify: (pin: string) => {
       const encrypted = Utils.signEd25519PIN(pin, keystore);
       return _axiosInstance.post<void>('/pin/verify', { pin: encrypted });
     },
 
     // Change the PIN of the user, or setup a new PIN if it is not set yet
-    update(oldPin: string, pin: string) {
-      let encryptedOldPin = '';
-      if (oldPin !== '') {
-        encryptedOldPin = Utils.signEd25519PIN(oldPin, keystore);
-      }
+    update: (pin: string, oldPin?: string) => {
+      const encryptedOldPin = oldPin ? Utils.signEd25519PIN(oldPin, keystore) : '';
       const encrypted = Utils.signEd25519PIN(pin, keystore);
       return _axiosInstance.post<void>('/pin/update', { old_pin: encryptedOldPin, pin: encrypted });
     },
