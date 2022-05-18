d3f72d6fe1ae583f24d78f72c775f176f4fa26c5
LiYuqing <im.yuqlee@gmail.com>
2022-05-18 12:01:26 +0800

    Revert user token client

--- src/newClient/user.ts
+++ src/newClient/user.ts
@@ -1,38 +1,56 @@
+import { AxiosInstance } from 'axios';
+import { pki } from 'node-forge';
+import { request } from '../services/request';
+import { BaseClient } from './types/client';
 import { AuthenticationUserResponse, UserResponse, RelationshipRequest } from './types/user';
-import { ErrorResponse } from './types/error';
-import Keystore from './types/keystore';
-import HTTP from './http';
 
 // Methods to manage user's information
-export const UserTokenClient = (keystore: Keystore) => {
-  const http = new HTTP(keystore);
-
+export const UserTokenClient = (axiosInstance: AxiosInstance) => {
+  async function create(fullName: string): Promise<AuthenticationUserResponse>;
+  async function create(fullName: string, sessionSecret: string): Promise<AuthenticationUserResponse>;
+  async function create(fullName: string, sessionSecret?: string) {
+    if (sessionSecret) {
+      return axiosInstance.post('/users', { full_name: fullName, session_secret: sessionSecret });
+    }
+    // todo export to utils?
+    const { publicKey, privateKey } = pki.ed25519.generateKeyPair();
+    const params = {
+      full_name: fullName,
+      session_secret: Buffer.from(publicKey).toString('base64'),
+    };
+    const user = await axiosInstance.post<unknown, AuthenticationUserResponse>('/users', params);
+    user.public_key = publicKey.toString('base64');
+    user.private_key = privateKey.toString('base64');
+    return user;
+  }
   return {
     // Get the current user's personal information
-    profile: (): Promise<AuthenticationUserResponse | ErrorResponse> => http.request('GET', `/me`),
+    profile: () => axiosInstance.get<unknown, AuthenticationUserResponse>(`/me`),
 
-    // Getting some user information by userID or identity_number
-    user: (id: string): Promise<UserResponse | ErrorResponse> => http.request('GET', `/users/${id}`),
-
-    // Getting users' information by user IDs in bulk
-    users: (userIDs: string[]): Promise<UserResponse[] | ErrorResponse> => http.request('POST', `/users/fetch`, userIDs),
+    // Getting user information by userID or identity_number
+    show: (userIdOrIdentityNumber: string) => axiosInstance.get<unknown, AuthenticationUserResponse | undefined>(`/users/${userIdOrIdentityNumber}`),
 
     // Getting users' block list
-    blockingUsers: (): Promise<UserResponse[] | ErrorResponse> => http.request('GET', `/blocking_users`),
+    blockingUsers: () => axiosInstance.get<unknown, UserResponse[]>(`/blocking_users`),
+
+    // Getting users' information by user IDs in bulk
+    showUsers: (userIDs: string[]) => axiosInstance.post<unknown, UserResponse[]>(`/users/fetch`, userIDs),
 
     // Obtaining the contact list of the users, containing users and bots
-    friends: (): Promise<UserResponse[] | ErrorResponse> => http.request('GET', `/friends`),
+    friends: () => axiosInstance.get<unknown, UserResponse[]>(`/friends`),
 
-    // Search users by keyword, identity can be mixin id, phone number or user id
-    search: (identity: string): Promise<UserResponse | ErrorResponse> => http.request('GET', `/search/${identity}`),
+    // Search users by keyword
+    search: (identityNumberOrPhone: string) => axiosInstance.get<unknown, UserResponse | undefined>(`/search/${identityNumberOrPhone}`),
 
     // Create a network user
-    createBareUser: (fullName: string, sessionSecret: string): Promise<AuthenticationUserResponse | ErrorResponse> => http.request('POST', '/users', { full_name: fullName, session_secret: sessionSecret}),
+    create,
 
     // Modify current user's personal name and avatar
-    update: (fullName: string, avatarBase64: string): Promise<AuthenticationUserResponse | ErrorResponse> => http.request('POST', `/me`, { full_name: fullName, avatar_base64: avatarBase64 }),
+    update: (fullName: string, avatarBase64: string) => axiosInstance.post<unknown, UserResponse>(`/me`, { full_name: fullName, avatar_base64: avatarBase64 }),
 
     // Manage the relationship between two users
-    updateRelationships: (relationship: RelationshipRequest): Promise<UserResponse | ErrorResponse> => http.request('POST', `/relationships`, relationship),
+    updateRelationships: (relationship: RelationshipRequest) => axiosInstance.post<unknown, UserResponse>(`/relationships`, relationship),
   };
 };
+
+export const UserClient: BaseClient<ReturnType<typeof UserTokenClient>, ReturnType<typeof UserTokenClient>> = (arg: any): any => UserTokenClient(request(arg));
