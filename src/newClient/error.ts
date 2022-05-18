ea90dbe21897f3a187716bfd6a7f8767e3963458
LiYuqing <im.yuqlee@gmail.com>
2022-05-18 18:55:22 +0800

    Fix requestId

--- src/newClient/error.ts
+++ src/newClient/error.ts
@@ -1,8 +1,5 @@
 export class ResponseError extends Error {
-
-  requestId: string
-
-  constructor(public code: number, public description: string, public status: number, public extra: unknown, requestId: string, public originalError: unknown) {
+  constructor(public code: number, public description: string, public status: number, public extra: object, public requestId: string, public originalError: unknown) {
     super(`code: ${code}, description: ${description}, status: ${status}, extra: ${extra}, requestId: ${requestId} originalError: ${originalError}`);
 
     this.code = code;
