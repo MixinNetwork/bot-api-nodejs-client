ec7f19c873e0a1cb80b32e8b1205245e5e707c6e
LiYuqing <im.yuqlee@gmail.com>
2022-05-19 10:50:11 +0800

    Fix utils tests

--- test/mixin/utils.test.ts
+++ test/mixin/utils.test.ts
@@ -1,38 +1,38 @@
-import Utils from '../../src/newClient/utils/utils';
+import Utils from '../../src/newClient/utils';
 
 describe('Tests for utils', () => {
   test('base64 encode & decode should be url safe', () => {
     // buffer to base64
-    expect(Utils.base64RawURLEncode(Buffer.from('a'))).toMatch('YQ');
-    expect(Utils.base64RawURLEncode(Buffer.from('ab'))).toMatch('YWI');
-    expect(Utils.base64RawURLEncode(Buffer.from('abcde'))).toMatch('YWJjZGU');
-    expect(Utils.base64RawURLEncode(Buffer.from('abcdefghijklmnopqrstuvwxyz'))).toMatch('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
+    expect(Utils.base64.RawURLEncode(Buffer.from('a'))).toMatch('YQ');
+    expect(Utils.base64.RawURLEncode(Buffer.from('ab'))).toMatch('YWI');
+    expect(Utils.base64.RawURLEncode(Buffer.from('abcde'))).toMatch('YWJjZGU');
+    expect(Utils.base64.RawURLEncode(Buffer.from('abcdefghijklmnopqrstuvwxyz'))).toMatch('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
 
     // string to base64
-    expect(Utils.base64RawURLEncode('abcdefghijklmnopqrstuvwxyz')).toMatch('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
+    expect(Utils.base64.RawURLEncode('abcdefghijklmnopqrstuvwxyz')).toMatch('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
 
     // empty string to base64
-    expect(Utils.base64RawURLEncode('')).toMatch('');
+    expect(Utils.base64.RawURLEncode('')).toMatch('');
 
     // base64 string to buffer
-    let buf = Utils.base64RawURLDecode('YQ');
+    let buf = Utils.base64.RawURLDecode('YQ');
     expect(buf.toString()).toMatch('a');
-    buf = Utils.base64RawURLDecode('YWI');
+    buf = Utils.base64.RawURLDecode('YWI');
     expect(buf.toString()).toMatch('ab');
-    buf = Utils.base64RawURLDecode('YWJjZGU');
+    buf = Utils.base64.RawURLDecode('YWJjZGU');
     expect(buf.toString()).toMatch('abcde');
-    buf = Utils.base64RawURLDecode('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
+    buf = Utils.base64.RawURLDecode('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo');
     expect(buf.toString()).toMatch('abcdefghijklmnopqrstuvwxyz');
 
     // base64 buffer to string
-    buf = Utils.base64RawURLDecode(Buffer.from('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo'));
+    buf = Utils.base64.RawURLDecode(Buffer.from('YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo'));
     expect(buf.toString()).toMatch('abcdefghijklmnopqrstuvwxyz');
   });
 
   test('tests for hashMembers', () => {
-    let hash = Utils.hashMembers(['965e5c6e-434c-3fa9-b780-c50f43cd955c']);
+    let hash = Utils.multisig.hashMembers(['965e5c6e-434c-3fa9-b780-c50f43cd955c']);
     expect(hash).toBe('b9f49cf777dc4d03bc54cd1367eebca319f8603ea1ce18910d09e2c540c630d8');
-    hash = Utils.hashMembers(['965e5c6e-434c-3fa9-b780-c50f43cd955c', 'd1e9ec7e-199d-4578-91a0-a69d9a7ba048']);
+    hash = Utils.multisig.hashMembers(['965e5c6e-434c-3fa9-b780-c50f43cd955c', 'd1e9ec7e-199d-4578-91a0-a69d9a7ba048']);
     expect(hash).toBe('6064ec68a229a7d2fe2be652d11477f21705a742e08b75564fd085650f1deaeb');
   });
 });
