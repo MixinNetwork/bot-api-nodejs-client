15fcb8e24008482dbe765cffd6bb9d19874162dd
LiYuqing <im.yuqlee@gmail.com>
2022-05-19 16:17:34 +0800

    Fix utils

--- src/newClient/utils/multisig.ts
+++ src/newClient/utils/multisig.ts
@@ -1,13 +1,26 @@
 import JsSHA from 'jssha';
+import forge from 'node-forge';
+import { stringify as uuidStringify } from 'uuid';
 
 // Supporting multisig for tokens & collectibles
-class Multisig {
-  static hashMembers(ids: string[]): string {
-    const key = ids.sort().join('');
-    const sha = new JsSHA('SHA3-256', 'TEXT', { encoding: 'UTF8' });
-    sha.update(key);
-    return sha.getHash('HEX');
-  }
+export const hashMembers = (ids: string[]): string => {
+  const key = ids.sort().join('');
+  const sha = new JsSHA('SHA3-256', 'TEXT', { encoding: 'UTF8' });
+  sha.update(key);
+  return sha.getHash('HEX');
 }
 
-export default Multisig;
+export const uniqueConversationID = (userID: string, recipientID: string): string => {
+  const [minId, maxId] = [userID, recipientID].sort();
+  const md5 = forge.md.md5.create();
+  md5.update(minId);
+  md5.update(maxId);
+  const bytes = Buffer.from(md5.digest().bytes(), 'binary');
+
+  bytes[6] = (bytes[6] & 0x0f) | 0x30;
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
+
+  return uuidStringify(bytes);
+};
+
+export default {};
