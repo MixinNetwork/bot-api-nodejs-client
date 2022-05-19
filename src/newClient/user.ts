a3488a722fb3191ce6c513e56fb16aba08a1012e
hundredark <hundredark@gmail.com>
2022-05-19 10:58:08 +0800

    Fix user commet

--- src/newClient/user.ts
+++ src/newClient/user.ts
@@ -2,21 +2,21 @@ import { AxiosInstance } from 'axios';
 import { AuthenticationUserResponse, UserResponse, RelationshipRequest } from './types/user';
 import { buildClient } from './utils/client';
 
-// Methods to manage user's information
+// Get users' information
 export const UserTokenClient = (axiosInstance: AxiosInstance) => ({
   // Get the current user's personal information
   profile: () => axiosInstance.get<unknown, AuthenticationUserResponse>(`/me`),
 
-  // Getting user information by userID or identity_number
+  // Get user information by userID or identity_number
   fetch: (userIdOrIdentityNumber: string) => axiosInstance.get<unknown, AuthenticationUserResponse>(`/users/${userIdOrIdentityNumber}`),
 
-  // Getting users' information by user IDs in bulk
+  // Get users' information by user IDs in bulk
   fetchList: (userIDs: string[]) => axiosInstance.post<unknown, UserResponse[]>(`/users/fetch`, userIDs),
 
-  // Getting users' block list
+  // Get users' block list
   blockings: () => axiosInstance.get<unknown, UserResponse[]>(`/blocking_users`),
 
-  // Obtaining the contact list of the users, containing users and bots
+  // Get the contact list of the users, containing users and bots
   friends: () => axiosInstance.get<unknown, UserResponse[]>(`/friends`),
 
   // Search users by keyword
