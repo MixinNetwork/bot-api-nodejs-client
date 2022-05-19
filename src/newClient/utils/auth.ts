83c702d850997cc8c40b2274f0bda5e87393e4f2
LiYuqing <im.yuqlee@gmail.com>
2022-05-19 13:14:50 +0800

    Fix auth

--- src/newClient/utils/auth.ts
+++ src/newClient/utils/auth.ts
@@ -1,7 +1,10 @@
 import { sharedKey } from 'curve25519-js';
 import forge from 'node-forge';
 import LittleEndian from 'int64-buffer';
+import serialize from 'serialize-javascript';
+import { v4 as uuid } from 'uuid';
 import Keystore from '../types/keystore';
+import Base64 from './base64';
 
 class Auth {
   static privateKeyToCurve25519(privateKey: Buffer) {
@@ -58,6 +61,46 @@ class Auth {
 
     const encryptedBytes = Buffer.from(pinBuff.getBytes(), 'binary');
     return forge.util.binary.base64.encode(encryptedBytes);
+  }
+
+  static signAuthenticationToken(requestMethod: string, uri: string, params: Object | string, keystore: Keystore) {
+    const method = requestMethod.toLocaleUpperCase();
+    let data: string = '';
+    if (typeof params === 'object') {
+      data = serialize(params);
+    } else if (typeof params === 'string') {
+      data = params;
+    }
+
+    const iat = Math.floor(Date.now() / 1000);
+    const exp = iat + 3600;
+    const md = forge.md.sha256.create();
+    md.update(method + uri + data, 'utf8');
+
+    const payload = {
+      uid: keystore.user_id,
+      sid: keystore.session_id,
+      iat,
+      exp,
+      jti: uuid(),
+      sig: md.digest().toHex(),
+      scp: keystore.scope || 'FULL',
+    };
+
+    const header = Base64.RawURLEncode(serialize({ alg: 'EdDSA', typ: 'JWT' }));
+    const payloadStr = Base64.RawURLEncode(serialize(payload));
+
+    const privateKey = Base64.RawURLDecode(keystore.private_key);
+    const result = [header, payloadStr];
+    const signData = forge.pki.ed25519.sign({
+      message: result.join('.'),
+      encoding: 'utf8',
+      privateKey,
+    });
+
+    const sign = Base64.RawURLEncode(Buffer.from(signData));
+    result.push(sign);
+    return result.join('.');
   }
 }
 
