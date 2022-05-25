546ea909ff02a23e8af149d5f058411ee6beffdb
LiYuqing <im.yuqlee@gmail.com>
2022-05-25 17:40:35 +0800

    Slight fix

--- src/newClient/types/transaction.ts
+++ src/newClient/types/transaction.ts
@@ -13,25 +13,6 @@ export interface RawTransactionRequest {
   pin?: string;
 }
 
-export interface RawTransactionResponse {
-  type: string;
-  snapshot: string;
-  opponent_key: string;
-  opponent_receivers: string[];
-  opponent_threshold: number;
-  asset_id: string;
-  amount: string;
-  opening_balance: string;
-  closing_balance: string;
-  trace_id: string;
-  memo: string;
-  state: string;
-  created_at: Date;
-  transaction_hash: string;
-  snapshot_hash: string;
-  snapshot_at: Date;
-}
-
 export interface GhostInput {
   receivers: string[];
   index: number;
