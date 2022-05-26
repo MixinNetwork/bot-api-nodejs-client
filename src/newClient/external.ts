96d43981547256d18cab3491c5333f971460f8c7
hundredark <hundredark@gmail.com>
2022-05-26 13:45:11 +0800

    Fix external

--- src/newClient/external.ts
+++ src/newClient/external.ts
@@ -4,13 +4,13 @@ import { buildClient } from './utils/client';
 
 export const ExternalKeystoreClient = (axiosInstance: AxiosInstance) => ({
   // Get public network-wide deposit records
-  deposit: (params: DepositRequest): Promise<ExternalTransactionResponse[]> => axiosInstance.get<unknown, ExternalTransactionResponse[]>('/external/transactions', { params }),
+  deposits: (params: DepositRequest): Promise<ExternalTransactionResponse[]> => axiosInstance.get<unknown, ExternalTransactionResponse[]>('/external/transactions', { params }),
 
   // Check if an address belongs to Mixin
-  externalAddressesCheck: (params: CheckAddressRequest): Promise<CheckAddressResponse> => axiosInstance.get(`/external/addresses/check`, { params }),
+  checkAddress: (params: CheckAddressRequest): Promise<CheckAddressResponse> => axiosInstance.get<unknown, CheckAddressResponse>(`/external/addresses/check`, { params }),
 
   // GET the list of all fiat exchange rates based on US Dollar
-  exchangeRates: (): Promise<ExchangeRateResponse> => axiosInstance.get<unknown, ExchangeRateResponse>('/external/fiats')
+  exchangeRates: (): Promise<ExchangeRateResponse[]> => axiosInstance.get<unknown, ExchangeRateResponse[]>('/external/fiats')
 });
 
 export const ExternalClient = buildClient(ExternalKeystoreClient);
