6a1a035aef473759d1a7a29c6e87d1dc98b44e59
LiYuqing <im.yuqlee@gmail.com>
2022-05-26 09:58:38 +0800

    Fix typo

--- src/newClient/code.ts
+++ src/newClient/code.ts
@@ -3,7 +3,7 @@ import { CodeResponse } from './types/code';
 import { buildClient } from './utils/client';
 
 export const CodeKeystoreClient = (axiosInstance: AxiosInstance) => ({
-  fetch: (consID: string): Promise<CodeResponse> => axiosInstance.get<unknown, CodeResponse>(`/codes/${consID}`),
+  fetch: (codeID: string): Promise<CodeResponse> => axiosInstance.get<unknown, CodeResponse>(`/codes/${codeID}`),
 });
 
 export const CodeClient = buildClient(CodeKeystoreClient);
