43600ea21bf2a7b8275dc04226561f3caf394ce0
LiYuqing <im.yuqlee@gmail.com>
2022-05-31 14:19:20 +0800

    Fix name CollectibleGenerateResponse -> CollectibleTransactionResponse

--- src/newClient/collectible.ts
+++ src/newClient/collectible.ts
@@ -6,8 +6,8 @@ import {
   CollectionResponse,
   CollectibleOutputsRequest,
   CollectibleOutputsResponse,
-  CollectibleGenerateRequest,
-  CollectibleGenerateResponse
+  CollectibleTransactionRequest,
+  CollectibleTransactionResponse
 } from './types/collectible';
 import { signEd25519PIN } from './utils/auth';
 import { buildClient } from './utils/client';
@@ -19,9 +19,9 @@ import { buildClient } from './utils/client';
 // * only uncompleted sign transaction can be unlocked.
 // Docs: https://developers.mixin.one/docs/api/collectibles/request
 export const CollectibleKeystoreClient = (axiosInstance: AxiosInstance, keystore: Keystore | undefined) => {
-  const manageRequest = (pin: string, requestID: string, action: CollectibleRequestAction): Promise<CollectibleGenerateResponse> => {
+  const manageRequest = (pin: string, requestID: string, action: CollectibleRequestAction): Promise<CollectibleTransactionResponse> => {
     const encrypted = signEd25519PIN(pin, keystore);
-    return axiosInstance.post<unknown, CollectibleGenerateResponse>(`/collectibles/requests/${requestID}/${action}`, { pin: encrypted });
+    return axiosInstance.post<unknown, CollectibleTransactionResponse>(`/collectibles/requests/${requestID}/${action}`, { pin: encrypted });
   };
 
   return {
@@ -34,8 +34,8 @@ export const CollectibleKeystoreClient = (axiosInstance: AxiosInstance, keystore
     // Get collectibles outputs
     outputs: (params: CollectibleOutputsRequest): Promise<CollectibleOutputsResponse> => axiosInstance.get<unknown, CollectibleOutputsResponse>('/collectibles/outputs', { params }),
 
-    // Generate a collectibles transfer request
-    request: (data: CollectibleGenerateRequest): Promise<CollectibleGenerateResponse> => axiosInstance.post<unknown, CollectibleGenerateResponse>('/collectibles/requests', data),
+    // Create a collectibles transfer request
+    request: (data: CollectibleTransactionRequest): Promise<CollectibleTransactionResponse> => axiosInstance.post<unknown, CollectibleTransactionResponse>('/collectibles/requests', data),
 
     // Initiate or participate in signing
     sign: (pin: string, requestID: string) => manageRequest(pin, requestID, 'sign'),
