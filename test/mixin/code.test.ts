20036f67717673758f9c0d999b8bddb8e280e5b0
LiYuqing <im.yuqlee@gmail.com>
2022-06-01 16:24:37 +0800

    Fix typo

--- test/mixin/code.test.ts
+++ test/mixin/code.test.ts
@@ -1,6 +1,6 @@
 import Code from '../../src/client/code';
 
-describe('Tests for users', () => {
+describe('Tests for codes', () => {
   const code = Code({});
   test('Test for fetch conversation', async () => {
     const resp = await code.fetch('50d49416-dcf5-4f8b-ae98-b7836a26a84a');
