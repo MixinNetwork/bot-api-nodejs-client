e7436a71608d8ac6082ec55fa91f0adb411336c9
LiYuqing <im.yuqlee@gmail.com>
2022-06-01 15:37:34 +0800

    Revert keystore

--- src/client/types/keystore.ts
+++ src/client/types/keystore.ts
@@ -1,11 +1,18 @@
 export interface Keystore {
   user_id: string;
-  session_id: string;
   private_key: string;
-  pin: string;
-  pin_token: string;
-  client_secret: string;
+
+  // oauth
   scope?: string;
+  authorization_id?: string;
+
+  // owner
+  session_id?: string;
+  pin?: string;
+  pin_token?: string;
+  client_secret?: string;
+
+  sign: 'oauth' | 'owner';
 }
 
 export default Keystore;
