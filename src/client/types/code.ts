81b43878614b31e5f431052e83e5e9c6369af8f0
LiYuqing <im.yuqlee@gmail.com>
2022-06-06 15:07:14 +0800

    Fix error import

--- src/client/types/code.ts
+++ src/client/types/code.ts
@@ -1,7 +1,7 @@
 import { ConversationResponse } from './conversation';
 import { MultisigRequestResponse } from './multisig';
 import { CollectibleTransactionResponse } from './collectible';
-import { PaymentRequestResponse } from './transfer';
+import { PaymentRequestResponse } from './payment';
 import { UserResponse } from './user';
 
 export type CodeResponse = ConversationResponse | MultisigRequestResponse | CollectibleTransactionResponse | PaymentRequestResponse | UserResponse;
