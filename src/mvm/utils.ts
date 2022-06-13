242d221f6caa3d320a357d836a6ce572437928e7
hundredark <hundredark@gmail.com>
2022-06-13 11:45:12 +0800

    Add extra

--- src/mvm/utils.ts
+++ src/mvm/utils.ts
@@ -1,6 +1,6 @@
-import { ethers } from 'ethers';
+import {ethers} from 'ethers';
+import {base64RawURLEncode} from 'client/utils/base64';
 import Encoder from './encoder';
-import { base64RawURLEncode } from '../client/utils/base64';
 
 // const OperationPurposeUnknown = 0
 const OperationPurposeGroupEvent = 1;
@@ -27,4 +27,24 @@ export const encodeMemo = (extra: string, process: string): string => {
 export const getMethodIdByAbi = (func: string, params: string[]): string => {
   const paramStr =  params.map((i) => i.trim()).join(',');
   return ethers.utils.id(`${func}(${paramStr})`).slice(2, 10);
+};
+
+// Extra consists of
+// 1 contract address without '0x'
+// 2 top 8 characters(without '0x') of KECCAK256 hash for contract function name and function parameter types
+//   e.g., addLiquidity(address,uint256)
+// 3 abi code(without '0x') of contract function arguments, if arguments exist
+export const getExtra = (contract: string, methodName: string, types: string[] = [], values: any[] = []) => {
+  if (types.length !== values.length) return '';
+
+  const contractAddress = contract.startsWith('0x') ? contract.slice(2) : contract;
+  const methodId = getMethodIdByAbi(methodName, types);
+  let extra = `${contractAddress}${methodId}`;
+
+  if (values.length > 0) {
+    const abiCoder = new ethers.utils.AbiCoder();
+    extra += abiCoder.encode(types, values).slice(2);
+  }
+
+  return extra;
 };
