cc791fa37c3093ab6a384a57c625a467f7abedaf
hundredark <hundredark@gmail.com>
2022-06-17 16:02:22 +0800

    Turn off rollup cache

--- tsdx.config.js
+++ tsdx.config.js
@@ -4,6 +4,8 @@ const replace = require('@rollup/plugin-replace');
 module.exports = {
   // This function will run for each entry/format/env combination
   rollup(config, opts) {
+    config.cache = false;
+
     config.plugins = config.plugins.map(p =>
       p.name === 'replace'
         ? replace({
