29576f8bccf69f60dd2a2667e604297d9cfed9de
hundredark <hundredark@gmail.com>
2022-06-23 17:09:59 +0800

    Fix different sharedKeys from browser and nodejs

--- src/client/utils/pin.ts
+++ src/client/utils/pin.ts
@@ -5,11 +5,13 @@ import Keystore from '../types/keystore';
 import { base64RawURLEncode } from './base64';
 
 const privateKeyToCurve25519 = (privateKey: Buffer) => {
-  const seed = privateKey.subarray(0, 32);
-  const md = forge.md.sha512.create();
-  md.update(seed.toString('binary'));
-  const digest = Buffer.from(md.digest().getBytes(), 'binary');
+  const seed = forge.util.createBuffer(privateKey.subarray(0, 32), 'raw');
 
+  const md = forge.md.sha512.create();
+  md.update(seed.getBytes());
+  const res = md.digest().getBytes();
+
+  const digest = Buffer.from(res, 'binary');
   digest[0] &= 248;
   digest[31] &= 127;
   digest[31] |= 64;
@@ -31,8 +33,8 @@ export const signEd25519PIN = (pin: string, keystore: Keystore | undefined): str
   const blockSize = 16;
 
   const _pin = Buffer.from(pin, 'utf8');
-  const iterator = Buffer.from(new Uint64(Math.floor(Date.now() / 1000)).toBuffer());
-  const time = Buffer.from(new Uint64(Math.floor(Date.now() / 1000)).toBuffer());
+  const iterator = Buffer.from(new Uint64(Date.now() * 1000000).toBuffer());
+  const time = Buffer.from(new Uint64(Date.now() / 1000).toBuffer());
   const buf = Buffer.concat([_pin, time, iterator]);
 
   const buffer = forge.util.createBuffer(buf.toString('binary'));
@@ -43,7 +45,7 @@ export const signEd25519PIN = (pin: string, keystore: Keystore | undefined): str
   }
   buffer.putBytes(Buffer.from(paddings).toString('binary'));
 
-  const iv = forge.random.getBytesSync(16);
+  const iv = forge.random.getBytesSync(blockSize);
   const sharedKey = sharedEd25519Key(keystore.pin_token!, keystore.private_key!);
   const cipher = forge.cipher.createCipher('AES-CBC', forge.util.createBuffer(sharedKey, 'raw'));
   cipher.start({ iv });
