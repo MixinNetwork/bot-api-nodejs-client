4c32e53a18c6fa6f48b6fb50bf88f7c5385a4e68
YeungKC <I@YeungKC.com>
2022-06-26 06:11:23 +0800

    fix export external

--- src/client/types/external.ts
+++ src/client/types/external.ts
@@ -1,14 +1,3 @@
-export interface CheckAddressRequest {
-  asset: string;
-  destination: string;
-  tag?: string;
-}
-
-export interface CheckAddressResponse {
-  destination: string;
-  tag: string;
-}
-
 export interface DepositRequest {
   asset?: string;
   destination?: string;
@@ -16,25 +5,6 @@ export interface DepositRequest {
   order?: 'ASC' | 'DESC';
   offset?: string;
   limit?: number;
-}
-
-export interface ExchangeRateResponse {
-  code: string;
-  rate: string;
-}
-
-export interface ExternalTransactionResponse {
-  transaction_id: string;
-  created_at: string;
-  transaction_hash: string;
-  sender: string;
-  chain_id: string;
-  asset_id: string;
-  amount: string;
-  destination: string;
-  tag: string;
-  confirmations: string;
-  threshold: string;
 }
 
 export interface ProxyRequest {
