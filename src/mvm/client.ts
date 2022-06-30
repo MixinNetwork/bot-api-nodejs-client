e604dc3add8d73336b9b7fbc1d2774088aea1aab
hundredark <hundredark@gmail.com>
2022-06-30 23:53:01 +0800

    Fix mvm client interceptors

--- src/mvm/client.ts
+++ src/mvm/client.ts
@@ -1,9 +1,9 @@
 import axios, {AxiosResponse} from 'axios';
-import { ResponseError, PaymentRequestResponse } from '../client';
-import { PaymentRequest, ValueResponse } from './types';
+import {PaymentRequestResponse} from '../client';
+import {PaymentRequest, ValueResponse} from './types';
 
 // Client supplies some APIs for mvm developer
-// * POST /payment, generate a payment code url for mixin user
+// * POST /payments, generate a payment code url for mixin user
 // * POST /values,
 //   The maximum memo size in mixin payment is 200, but in many cases the extra length will be greater than 200.
 //   We supply a method that put the long extra in contract.
@@ -16,11 +16,7 @@ export const MVMApi = (uri: string) => {
   });
 
   instance.interceptors.response.use(
-    async (res: AxiosResponse) => {
-      const { data, error } = res.data;
-      if (error) throw new ResponseError(error.code, error.description, error.status, error.extra, res.headers['X-Request-Id'], error);
-      return data;
-    }
+    async (res: AxiosResponse) => res.data,
   );
 
   return {
