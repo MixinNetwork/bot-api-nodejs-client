5fcdaac1f5b0309a51b5fbf1d192c04f97c3525a
LiYuqing <im.yuqlee@gmail.com>
2022-07-05 12:07:48 +0800

    More comments

--- src/mvm/bridge/client.ts
+++ src/mvm/bridge/client.ts
@@ -15,6 +15,14 @@ export const BridgeApi = (uri: string = 'https://bridge.mvm.dev') => {
      * example: wallet.signMessage(keccak256(toUtf8Bytes(`MVM:Bridge:Proxy:${server_public_key_base64}:${address}`))).slice(2)
      */
     register: async (params: RegisterRequest) => (await instance.post<undefined, { user: RegisteredUser }>('/users', params)).user,
+    /**
+     * destination: string, btc address (3MzLbnrkL8HV5tbuo....z8qMyBw7R33) or eos account name (eoswithmixin)
+     * tag: string, like eos memo
+     * receivers: []string, mixin user id set
+     * threshold: Number
+     * extra: string
+     * all params are optional
+     */
     generateExtra: async (params: GenerateExtraRequest) => `0x${(await instance.post<undefined, { extra: string }>('/extra', params)).extra}`,
   };
 };
