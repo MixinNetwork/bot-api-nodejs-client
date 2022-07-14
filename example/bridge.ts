39a19163cfec13f938997d4306c4efdd50e88f53
Crossle Song <crosslesong@gmail.com>
2022-07-14 21:02:19 +0400

    Fix typo

--- example/bridge.ts
+++ example/bridge.ts
@@ -1,7 +1,7 @@
 async function main() {
-    var bridgeApi = require('@mixin.dev/mixin-node-sdk');
-    var r = await bridgeApi.BridgeApi().register({ 'public_key': '0x5fCaB872A5B0056cD8649885425d9309d14AE7b7' })
-    console.log('mixin://transfer/'+ r.key.client_id)
+  var bridgeApi = require('@mixin.dev/mixin-node-sdk');
+  var r = await bridgeApi.BridgeApi().register({ public_key: '0x5fCaB872A5B0056cD8649885425d9309d14AE7b7' });
+  console.log('mixin://transfer/' + r.key.client_id);
 }
 
 main();
