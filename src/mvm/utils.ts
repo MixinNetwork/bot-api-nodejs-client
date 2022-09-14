5031e49ef72b3bf823145ef2bf51eb7c5ccb8787
hundredark <hundredark@gmail.com>
2022-09-14 23:57:49 +0800

    Fix format

--- src/mvm/utils.ts
+++ src/mvm/utils.ts
@@ -1,4 +1,4 @@
-import { ethers} from 'ethers';
+import { ethers } from 'ethers';
 import { ContractRequest } from './types';
 import { MixinAssetID } from '../constant';
 import { MVMMainnet } from './constant';
