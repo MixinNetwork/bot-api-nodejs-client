cffab54d795e26f7b0ebfd915b0cb4c72f536cb0
hundredark <hundredark@gmail.com>
2022-11-11 15:11:09 +0800

    Remove unused file

--- src/mixin/tools.ts
+++
@@ -1,6 +0,0 @@
-export function toBuffer(content: any, encoding: any = 'utf8') {
-  if (typeof content === 'object') {
-    content = JSON.stringify(content);
-  }
-  return Buffer.from(content, encoding);
-}
