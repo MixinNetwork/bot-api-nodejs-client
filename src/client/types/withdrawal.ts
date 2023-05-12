fc69686f6900c2bf47be41e849792ee153caa6ff
LiYuqing <im.yuqlee@gmail.com>
2023-05-12 19:53:40 +0800

    Slight fix

--- src/client/types/withdrawal.ts
+++ src/client/types/withdrawal.ts
@@ -1,7 +1,7 @@
 export interface WithdrawalRequest {
   address_id: string;
   amount: string;
-  pin_base64?: string;
+  pin_base64: string;
   fee?: string;
   trace_id?: string;
   memo?: string;
