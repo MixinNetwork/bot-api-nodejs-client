df310adf86234dc2fdfd8a4e0a971095b91b62a0
hundredark <hundredark@gmail.com>
2023-08-10 17:59:43 +0800

    Updata user_id as client_id if no user_id

--- src/client/types/keystore.ts
+++ src/client/types/keystore.ts
@@ -12,5 +12,20 @@ export interface Keystore {
   pin_token?: string;
   client_secret?: string;
 }
+interface ClientKeystore {
+  client_id: string;
+  private_key: string;
+
+  // oauth
+  scope?: string;
+  authorization_id?: string;
+
+  // owner private key
+  session_id?: string;
+  pin?: string;
+  pin_token?: string;
+  client_secret?: string;
+}
+export type InitKeystore = Keystore | ClientKeystore;
 
 export default Keystore;
