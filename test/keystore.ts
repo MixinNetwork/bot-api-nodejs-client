ea0336b9ce025d9cdcc718724fbff3f093913635
hundredark <hundredark@gmail.com>
2024-09-20 18:41:54 +0800

    format

--- test/keystore.ts
+++ test/keystore.ts
@@ -1,6 +1,6 @@
 const keystore = {
-  app_id: "4b79fe76-0d9d-49e6-85fd-0f6be01147da",
-  session_id: "a03a4496-3e59-4abd-bd1e-1ab8a2acb550",
+  app_id: '4b79fe76-0d9d-49e6-85fd-0f6be01147da',
+  session_id: 'a03a4496-3e59-4abd-bd1e-1ab8a2acb550',
   server_public_key: '413506980af07695d590bae4b58bafac9f381ab8c6ef02bef85fab3bee21de26',
   session_private_key: 'c838d99617cb2543bc6b7eb4ac0a1daede5d132ed1c5442fbbcad8baaf556748',
 };
